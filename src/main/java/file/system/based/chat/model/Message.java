434f407aa836032696cb48ca4d1efc96f85881d5
S-Maks <cool.dan47@mail.ru>
2021-03-04 21:16:33 +0300

    Add message entity

---
+++ src/main/java/file/system/based/chat/model/Message.java
@@ -0,0 +1,49 @@
+package file.system.based.chat.model;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Message {
+    private String content;
+    private int toUser;
+    private int sendUser;
+    private final String date = new SimpleDateFormat("yyyy.MM.dd-hh.mm.ss").format(new Date());
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getToUser() {
+        return toUser;
+    }
+
+    public void setToUser(int toUser) {
+        this.toUser = toUser;
+    }
+
+    public int getSendUser() {
+        return sendUser;
+    }
+
+    public void setSendUser(int sendUser) {
+        this.sendUser = sendUser;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "content='" + content + '\'' +
+                ", toUser=" + toUser +
+                ", sendUser=" + sendUser +
+                ", date='" + date + '\'' +
+                '}';
+    }
+}
